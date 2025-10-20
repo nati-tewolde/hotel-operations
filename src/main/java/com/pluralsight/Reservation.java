ecc2a26b4531f8488f351d0d625fdbd496b999e0
nati-tewolde <natisnewemail@gmail.com>
2025-10-20 13:57:35 -0400

    Add Reservation class - Add attributes, constructor, getters, setters, and derived getter

---
+++ src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,56 @@
+package com.pluralsight;
+
+public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice(){
+        double basePrice;
+        if (roomType.equalsIgnoreCase("king")) {
+            basePrice = 139;
+        } else {
+            basePrice = 124;
+        }
+
+        if (isWeekend) {
+            basePrice += basePrice * 0.1;
+        }
+
+        return basePrice;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getReservationTotal() {
+        return numberOfNights * getPrice();
+    }
+}
