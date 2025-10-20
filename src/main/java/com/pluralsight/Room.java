bb33bfb637f4e481f0a0a52a64ea05d18110df5c
nati-tewolde <natisnewemail@gmail.com>
2025-10-20 13:36:44 -0400

    Add Room class - Add attributes, constructor, getters, and derived getter

---
+++ src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class Room {
+    private int numberOfBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+
+    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public boolean isAvailable() {
+        return !isDirty && !isOccupied;
+    }
+}
