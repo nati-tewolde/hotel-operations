bb9f6a4fac1a648e779941a6dbea0e31a2344f18
nati-tewolde <natisnewemail@gmail.com>
2025-10-21 13:27:46 -0400

    Add Hotel class

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -1,4 +1,78 @@
 package com.pluralsight;
 
 public class Hotel {
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRooms;
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = 0;
+        this.numberOfRooms = 0;
+    }
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSuites() {
+        return numberOfSuites;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getBookedSuites() {
+        return bookedSuites;
+    }
+
+    public int getBookedBasicRooms() {
+        return bookedBasicRooms;
+    }
+
+    /*public boolean bookRoom(int numberOfRooms, boolean isSuite) {
+        if (isSuite) {
+            if (getAvailableSuites() >= numberOfRooms) {
+                bookedSuites += numberOfRooms;
+                return true;
+            }
+        } else {
+            if (getAvailableRooms() >= numberOfRooms) {
+                bookedBasicRooms += numberOfRooms;
+                return true;
+            }
+        }
+        return false;
+    }*/
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite) {
+        if (isSuite && getAvailableSuites() >= numberOfRooms) {
+            bookedSuites += numberOfRooms;
+            return true;
+        } else if (!isSuite && getAvailableRooms() >= numberOfRooms) {
+            bookedBasicRooms += numberOfRooms;
+            return true;
+        }
+        return false;
+    }
+
+    public int getAvailableSuites() {
+        return numberOfSuites - bookedSuites;
+    }
+
+    public int getAvailableRooms() {
+        return numberOfRooms - bookedBasicRooms;
+    }
+
 }
